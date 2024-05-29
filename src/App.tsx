174e7e54c5d9609635905a8b3ab191bb4de2ddbd
Reza Jaber <frontendrezajaber@gmail.com>
2024-05-29 23:35:07 +0200

    project setup | tailwind & shadcn

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from "solid-js";
+import { Button } from "@/components/ui/button";
+
+const App: Component = () => {
+  return (
+    <div>
+      <Button variant="outline">Button</Button>
+    </div>
+  );
+};
+
+export default App;
