d98290e05f9f9b22778336b27b9b4d521b826c06
Reza Jaber <frontendrezajaber@gmail.com>
2024-05-30 01:13:30 +0200

    Organize Files

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import type { Component } from "solid-js";
 import { Button } from "@/components/ui/button";
 
 const App: Component = () => {
-  return (
-    <div>
-      <Button variant="outline">Button</Button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
